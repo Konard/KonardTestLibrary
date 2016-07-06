6e6d14b913100a490c72a16312258b948b397817
Konstantin Dyachenko <drakonard@gmail.com>
2016-07-06 23:39:20 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KonardTestLibrary
+{
+    public class Library
+    {
+        public long Add(long first, long second)
+        {
+            return first + second;
+        }
+    }
+}
